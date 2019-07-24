683d7e7b9e3690ef21218b4b07c7433befcfce04
Nicholas <nicholasmarca@gmail.com>
2019-07-24 16:11:04 +0100

    added some components;

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+/** @jsx jsx */
+
+import { jsx, css, Global } from '@emotion/core'
+
+const globalCss = css`
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+`
 
 function App() {
-  return (
-    <div>
-      Hello world !!!
-    </div>
-  );
+    return (
+        <div>
+            <Global styles={globalCss} />
+            Hello world !!!
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ client/src/components/Item.js
@@ -0,0 +1,19 @@
+/** @jsx jsx */
+
+import { jsx, css } from '@emotion/core';
+
+const containerCss = css `
+  display: flex;
+  background: rgb(244, 220, 185);
+  height:  
+`
+
+function Item() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Item;
